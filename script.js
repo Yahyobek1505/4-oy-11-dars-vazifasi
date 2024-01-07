4c36640b3a0127527293764dcd42e9306cf19ba8
Yahyobek1505 <yahyohonorinboyev0105@gmail.com>
2024-01-07 20:06:29 +0500

    Homework is started.

---
+++ script.js
@@ -0,0 +1,50 @@
+// //<===============================> 1-masala <===============================>
+
+// let sozlar = "Najot Ta'lim, Javascrip, bolajon";
+
+// function string(arg) {
+//     let massiv = arg.split('');
+//     let massivSoz = massiv.reverse();
+//     let harflar = massivSoz.join('');
+
+//     return harflar;
+//   }
+
+//   console.log(string(sozlar));
+
+// //<===============================> 2-masala <===============================>
+
+// let uzunGap = "Javascript is the best web programming language!";
+
+// function string(arg) {
+//     let sozlar = arg.split(' ');
+//     let uzunSoz = '';
+
+//     for (let i = 0; i < sozlar.length; i++) {
+//       if (sozlar[i].length > uzunSoz.length) {
+//         uzunSoz = sozlar[i];
+//       }
+//     }
+
+//     return uzunSoz;
+//   }
+
+//   console.log(string(uzunGap));
+
+// //<===============================> 3-masala <===============================>
+
+// let unliGap = "unlilar nechta ekan?";
+// let unlilar = unliGap.split("");
+// let unliHarflar = ["a", "i", "o", "e", "u"];
+// let counter = 0;
+
+// function unliSana(arg) {
+//     for (const value of unlilar){
+//         if(unliHarflar.indexOf(value) != -1){
+//             counter++
+//         }
+//     }
+//     return counter
+// }
+
+// console.log("Unlilar soni " + unliSana(unliGap) + " ta");
